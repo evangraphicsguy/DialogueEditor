5350db7d099ea8a8be99362545ce611b909c04a0
evangraphicsguy <evangraphicsguy@gmail.com>
2021-09-08 23:36:32 -0700

    Initial commit

---
+++ Assets/Scripts/Dialogue/_Nodes/RandomChoiceNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ETools.Dialogue
+{
+	public class RandomChoiceNode : DialogueNode
+	{
+		public int[] randomWeighting;
+		public DialogueNode[] nextNodes;
+
+		public override string ToString()
+		{
+			string sum = "Random Choice Node - ";
+			foreach (int randomWeight in randomWeighting)
+			{
+				sum += randomWeight.ToString() + ", ";
+			}
+			return sum.TrimEnd().TrimEnd(',');
+		}
+	}
+}
